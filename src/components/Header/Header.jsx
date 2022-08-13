9eb570f76b056821a9b1d98dccab93d3129d1065
Tyler Beck <softwaretpd2035@gmail.com>
2022-08-13 15:53:55 -0400

    updated Header.js using materail ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Typography, InputBase, Box, Toolbar } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
     return(
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar >
+                <Typography variant="h5" >
+                    Travel Advisor 
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" >
+                        Search new places!
+                    </Typography>
+                    {/* <Autocomplete>
+                        <div >
+                            <div >
+                                <SearchIcon /> 
+                            </div>
+                            <InputBase placeholder="Search..." />
+                        </div>
+                    </Autocomplete>  */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
